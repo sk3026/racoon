5c90e55a869aa28c290fc27a89f7c0b9c06ff92e
suraj <sk3057448@gmail.com>
2026-02-16 11:30:20 +0530

    feat: add additional reducers for checkout, order, and admin management in the Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authslice";
 import productsReducer from "./slices/productslice";
 import cartReducer from "./slices/cartslice";
+import checkoutReducer from "./slices/checkoutslice";
+import orderReducer from "./slices/orderslice";
+import adminReducer from "./slices/adminslice"; 
+import adminProductsReducer from "./slices/adminproductslice";
+import adminOrderReducer from "./slices/adminorderslice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productsReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
+    order: orderReducer,
+    admin: adminReducer,
+    adminProducts: adminProductsReducer,
+    adminOrders: adminOrderReducer,
   },
 
   // DevTools enabled automatically in development
